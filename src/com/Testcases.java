fc863c2255a4b2d3c4b31acaab04fbc745e879e9
Dhanashree2023 <prachi123451@gmail.com>
2023-10-07 08:29:31 +0530

    testcase2

--- src/com/Testcases.java
+++ src/com/Testcases.java
@@ -5,6 +5,9 @@ public class Testcases {
 	public static void main(String[] args) {
 	
 		System.out.println("Test case 1");
+		
+		System.out.println("X tester updated code for user story");
+		System.out.println("test case2");
 
 	}
 
